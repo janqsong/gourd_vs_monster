30ebe84294df173e3dfd052ab3da7b0ae445e2d6
项仁浩 <2579348964@qq.com>
2020-12-31 21:15:24 +0800

    子弹

--- gourd_vs_monster/src/main/java/com/sjq/gourd/bullet/Bullet.java
+++ gourd_vs_monster/src/main/java/com/sjq/gourd/bullet/Bullet.java
@@ -12,13 +12,13 @@ import javafx.scene.shape.Circle;
 import org.checkerframework.checker.units.qual.C;
 
 public class Bullet {
-    private Creature sourceCreature;
-    private Creature targetCreature;
-    private ImagePosition imagePosition;
-    private Circle circleShape;
+    private final Creature sourceCreature;
+    private final Creature targetCreature;
+    private final ImagePosition imagePosition;
+    private final Circle circleShape;
+    private final int bulletType;
     private static final double SPEED = Constant.BULLET_SPEED;
     private boolean valid;
-    private int bulletType;
     private BulletState bulletState = BulletState.NONE;
 
     public Bullet(Creature sourceCreature, Creature targetCreature, int bulletType, BulletState bulletState) {
@@ -28,10 +28,10 @@ public class Bullet {
         this.bulletState = bulletState;
         this.valid = true;
         this.imagePosition = this.sourceCreature.getCenterPos();
-        if(this.bulletType == Constant.CLOSE_BULLET_TYPE)
+        if (this.bulletType == Constant.CLOSE_BULLET_TYPE)
             this.circleShape = null;//近战没有图形
-        else{
-            this.circleShape = new Circle(imagePosition.getLayoutX(),imagePosition.getLayoutY(),5);
+        else {
+            this.circleShape = new Circle(imagePosition.getLayoutX(), imagePosition.getLayoutY(), 5);
             this.circleShape.setVisible(false);
             int id = sourceCreature.getCreatureId();
             if (id == CreatureId.SNAKE_MONSTER_ID) {
@@ -41,7 +41,7 @@ public class Bullet {
                 //蝙蝠的
                 this.circleShape.setFill(Color.ROSYBROWN);
                 this.circleShape.setRadius(6);
-            }else if(id == CreatureId.MONSTER4_ID){
+            } else if (id == CreatureId.MONSTER4_ID) {
                 //蛤蟆精的
                 this.circleShape.setFill(Color.LIGHTYELLOW);
                 this.circleShape.setRadius(6);
@@ -56,10 +56,10 @@ public class Bullet {
             else if (id == CreatureId.GRANDPA_ID)
                 this.circleShape.setFill(Color.GREEN);
 
-            if(this.bulletState.equals(BulletState.THE_SON_OF_FLAME)
-                    ||this.bulletState.equals(BulletState.THE_HEART_OF_ICE)
-                    ||this.bulletState.equals(BulletState.THE_GOD_OF_HEALING_MAX)
-                    ||this.bulletState.equals(BulletState.THE_TEETH_OF_POISONOUS))
+            if (this.bulletState.equals(BulletState.THE_SON_OF_FLAME)
+                    || this.bulletState.equals(BulletState.THE_HEART_OF_ICE)
+                    || this.bulletState.equals(BulletState.THE_GOD_OF_HEALING_MAX)
+                    || this.bulletState.equals(BulletState.THE_TEETH_OF_POISONOUS))
                 this.circleShape.setRadius(10);
         }
     }
@@ -152,6 +152,8 @@ public class Bullet {
         this.valid = valid;
     }
 
+
+    //设置子弹可见状态
     public void setVisible(boolean visible) {
         if (circleShape != null) {
             Platform.runLater(new Runnable() {
@@ -163,39 +165,49 @@ public class Bullet {
         }
     }
 
+    //设置子弹位置
     public void setImagePosition(double layoutX, double layoutY) {
         imagePosition.setLayoutX(layoutX);
         imagePosition.setLayoutY(layoutY);
     }
 
+    //根据子弹当前位置绘制子弹
     public void draw() {
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
+                //目标死亡,子弹消失,无效
                 if (!targetCreature.isAlive()) {
                     circleShape.setVisible(false);
                     valid = false;
                     return;
                 }
+                //子弹有效,绘画,无效,不绘制
                 if (valid) {
                     circleShape.setVisible(true);
                     circleShape.setCenterX(imagePosition.getLayoutX());
                     circleShape.setCenterY(imagePosition.getLayoutY());
-                }
+                } else
+                    circleShape.setVisible(false);
             }
         });
     }
 
+    //子弹移动返回碰撞
     private Collision move() {
+        //获取目标中心位置
         ImagePosition targetPos = targetCreature.getCenterPos();
         double deltaX = targetPos.getLayoutX() - imagePosition.getLayoutX();
         double deltaY = targetPos.getLayoutY() - imagePosition.getLayoutY();
+        //根据偏移量计算角度
         double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
         double sin = deltaY / distance;
         double cos = deltaX / distance;
+        //根据速度和角度设置新的子弹位置
         imagePosition.setLayoutX(imagePosition.getLayoutX() + cos * SPEED);
         imagePosition.setLayoutY(imagePosition.getLayoutY() + sin * SPEED);
 
+        //子弹和目标图形相交发生碰撞
         if (circleShape.intersects(targetCreature.getCreatureImageView().getBoundsInParent())) {
             valid = false;
             return new Collision(this);
@@ -203,14 +215,14 @@ public class Bullet {
         return null;
     }
 
+    //子弹更新,返回碰撞,无碰撞返回null
     public Collision update() {
+        //远程子弹绘制并返回碰撞,近战子弹直接返回碰撞
         if (bulletType == Constant.REMOTE_BULLET_TYPE) {
             draw();
-            Collision collision = move();
-            return collision;
-        } else {
+            return move();
+        } else
             return new Collision(this);
-        }
     }
 
     public int getBulletType() {
